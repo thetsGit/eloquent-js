b40c69ec9e19c32c2e11d3f8a54043c0c0fae94e
Thet Lin Han <thetlinhan@dss.com.mm>
2023-11-18 15:53:03 +0630

    feat: implement custom loop

---
+++ hof/loop.js
@@ -0,0 +1,15 @@
+export const loop = (from, test, update, read) => {
+	let currentVal = from;
+
+	while (test(currentVal)) {
+		read(currentVal);
+		currentVal = update(currentVal);
+	}
+};
+
+loop(
+	3,
+	(n) => n > 0,
+	(n) => n - 1,
+	console.log
+);
