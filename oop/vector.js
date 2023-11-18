535174f8f4891778f35532cc5e2900ffd0afb9ed
Thet Lin Han <thetlinhan@dss.com.mm>
2023-11-18 15:57:20 +0630

    feat: implement custom object called 'vector'

---
+++ oop/vector.js
@@ -0,0 +1,19 @@
+class Vec {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	get length() {
+		// according to pythagorean theorem
+		return Math.sqrt(this.x ** 2 + this.y ** 2);
+	}
+
+	plus(vec) {
+		return new Vec(this.x + vec.x, this.y + vec.y);
+	}
+
+	minus(vec) {
+		return new Vec(this.x - vec.x, this.y - vec.y);
+	}
+}
