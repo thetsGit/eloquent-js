c89b3dab128c83e3e8d55e2c5357b4553295d0ce
Thet Lin Han <thetlinhan@dss.com.mm>
2023-11-18 15:58:56 +0630

    feat: apply method borrowing technique using 'call' to overcome prototype shadowing and name conflicts

---
+++ oop/mathodBorrowing.js
@@ -0,0 +1,10 @@
+// fixed version
+let map = { one: true, two: true, hasOwnProperty: true };
+
+// Fix this call
+console.log(map.hasOwnProperty("one"));
+// TypeError: map.hasOwnProperty is not a function
+
+// fixed
+console.log(Object.hasOwn(map, "one"));
+// → true
