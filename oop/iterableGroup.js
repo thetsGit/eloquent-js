abc620e40ef9ae929f1557dc231b1b3ad3335ca6
Thet Lin Han <thetlinhan@dss.com.mm>
2023-11-18 15:58:04 +0630

    feat: implement custom iterator object for Group data structure

---
+++ oop/iterableGroup.js
@@ -0,0 +1,27 @@
+import { Group } from "./group";
+
+class IterableGroup {
+	constructor(group) {
+		this.index = 0;
+		this.group = group;
+	}
+
+	next() {
+		if (this.index === this.group.length) {
+			return { done: true };
+		}
+
+		return {
+			value: this.group.get(this.index++),
+			done: false,
+		};
+	}
+}
+
+Group.prototype[Symbol.iterator] = function () {
+	return new IterableGroup(this);
+};
+
+for (let value of Group.from(["a", "b", "c"])) {
+	console.log(value);
+}
