cc1168091b88be06cd4c6f55de188145279197d6
Thet Lin Han <thethan.code@gmail.com>
2023-12-16 10:50:17 +0630

    refactor: improve naming

--- projects/robot/compareRobots.js
+++ projects/robot/compareRobots.js
@@ -6,20 +6,20 @@ const { runRobot } = require("./runRobot");
 const getFixedAverage = (total, count) => (total / count).toFixed(0);
 
 function compareRobots(robot1, memory1, robot2, memory2) {
-	let totalStepsByRobots = [0, 0];
+	let totalSteps = [0, 0];
 
 	console.time("complexity");
 
 	for (let i = 0; i < TOTAL_TASKS; i++) {
 		const state = VillageState.random();
 
-		totalStepsByRobots[0] += runRobot(state, robot1, memory1);
-		totalStepsByRobots[1] += runRobot(state, robot2, memory2);
+		totalSteps[0] += runRobot(state, robot1, memory1);
+		totalSteps[1] += runRobot(state, robot2, memory2);
 	}
 
 	console.timeEnd("complexity");
 
-	return totalStepsByRobots.map((totalStepsPerRobot) =>
+	return totalSteps.map((totalStepsPerRobot) =>
 		getFixedAverage(totalStepsPerRobot, TOTAL_TASKS)
 	);
 }
