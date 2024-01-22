f042e1438744578d13fb008cd3ae4504bda0a724
Thet Lin Han <thethan.code@gmail.com>
2024-01-22 22:35:56 +0630

    feat: bugs: implement lockedBox

---
+++ bugs/lockedBox.js
@@ -0,0 +1,41 @@
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(body) {
+  const isBoxLockedInitially = box.locked;
+
+  box.unlock();
+
+  try {
+    body();
+  } finally {
+    // reset the locked state back
+    isBoxLockedInitially && box.lock();
+  }
+}
+
+withBoxUnlocked(function () {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked);
+// → true
