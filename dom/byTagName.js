3db4a66a828fca773760b98132df2a97c3492f10
Thet Lin Han <thethan.code@gmail.com>
2024-11-09 15:56:56 +0700

    feat: exercise-14.2: implement byTagName

---
+++ dom/byTagName.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+
+function byTagName(node, tagName) {
+  const matchedNodes = [];
+
+  // If not Element type
+  if (node.nodeType !== 1) {
+    return matchedNodes;
+  }
+
+  if (node.children.length) {
+    // Convert to Array to use 'reduce'
+    const childrenArray = Array.from(node.children);
+
+    matchedNodes.push(
+      ...childrenArray.reduce((matched, currentNode) => {
+        if (currentNode.tagName.toLowerCase() === tagName) {
+          matchedNodes.push(currentNode);
+        }
+
+        return [...matched, ...byTagName(currentNode, tagName)];
+      }, []),
+    );
+  }
+
+  return matchedNodes;
+}
+
+console.log(byTagName(document.body, 'h1').length);
+// → 1
+console.log(byTagName(document.body, 'span').length);
+// → 3
+let para = document.querySelector('p');
+console.log(byTagName(para, 'span').length);
